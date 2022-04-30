24595523755d57f6a1a30eb80a698e88e928769f
dbronner18 <bronnerdevon@gmail.com>
2022-04-29 22:25:04 -0400

    Updates to watchlist sharing updates

--- app/src/main/java/com/example/movielist/WatchList.java
+++ app/src/main/java/com/example/movielist/WatchList.java
@@ -2,21 +2,30 @@ package com.example.movielist;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
-public class WatchList extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class WatchList extends AppCompatActivity {
     private SQLiteDatabase db;
     private Cursor cursor;
+
+    private ShareActionProvider shareActionProvider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +53,47 @@ public class WatchList extends AppCompatActivity {
             Toast toast = Toast.makeText(this, "Database Unavailable", Toast.LENGTH_SHORT);
             toast.show();
         }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Inflate the menu; this adds items to the app bar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ArrayList<String> shareList = readDB();
+        String shareText = "Check Out These Movies to Watch!\n";
+        for(int i = 0; i< shareList.size(); i++){
+            shareText = shareText + shareList.get(i) + "\n";
+        }
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent(shareText);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public ArrayList<String> readDB(){
+        SQLiteOpenHelper favoritesDatabaseHelper = new FavoritesDatabaseHelper(this);
+        ArrayList<String> shareList = new ArrayList<>();
+        try{
+            SQLiteDatabase  db = favoritesDatabaseHelper.getReadableDatabase();
+            cursor = db.rawQuery("SELECT * FROM WATCH", null);
+            if(cursor.moveToFirst()){
+                do{
+                    shareList.add(cursor.getString(1));
+                }while(cursor.moveToNext());
+            }
+
+        }
+        catch (SQLiteException e){
+            Toast toast = Toast.makeText(this, "Database Unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+        return shareList;
+    }
+
+    private void setShareActionIntent(String shareText){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, shareText);
+        shareActionProvider.setShareIntent(intent);
     }
 
     @Override
